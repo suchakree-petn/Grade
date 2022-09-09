5bf15e8b8ceea7027ffc7f9f146dd25ae9710c28
suchakree-petn <suchakree.petn@kmutt.ac.th>
2022-09-09 11:27:19 +0700

    Test

---
+++ User.cs
@@ -0,0 +1,31 @@
+public class User{
+    string[] Name = new string[8];
+    string[] SurName = new string[8];
+    string[] StdNum = new string[8];
+    string[] Grade = new string[8];
+
+    public void NameArray(){
+        for(int i =0; i<8; i++){
+            Name[i] = Console.ReadLine();
+            Console.WriteLine(Name.Length);
+        }
+    }
+    public void SurNameArray(){
+        for(int i =0; i<8; i++){
+            SurName[i] = Console.ReadLine();
+        }
+    }
+    public void StdNumArray(){
+        for(int i =0; i<8; i++){
+            StdNum[i] = Console.ReadLine();
+        }
+    }
+    public void GradeArray(){
+        for(int i =0; i<8; i++){
+            StdNum[i] = ;
+        }
+    }
+    
+    
+
+}
